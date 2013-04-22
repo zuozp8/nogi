20a008c8e37ae694cb631dc1283a2d87e4d3cbdc
Konrad Baumgart <zuo.zp8@gmail.com>
2013-04-22 21:01:48 +0200

    setCodecForCStrings nie używany w Qt5

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	#if QT_VERSION < 0x050000
 	QTextCodec::setCodecForCStrings((QTextCodec::codecForName("UTF-8")));
+	#endif
 	Visualiser* v = VISUALISER;
 	v->show();
 	
