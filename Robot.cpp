a427be57efe1727d0a2945d23abc751551b1bf15
oiah <kalina.jasinska@gmail.com>
2013-04-29 22:06:16 +0200

    poprawka 2pi radianow a nie pi radianow

--- Robot.cpp
+++ Robot.cpp
@@ -1,35 +1,41 @@
 #include "Robot.hpp"
 #include "Visualiser.hpp"
 #include <QDebug>
+#include <QTime>
+#include <QGlobal.h>
+#include <QDebug>
 
 Robot::Robot(QObject *parent) :
 	QObject(parent)
 {
+	QTime time = QTime::currentTime();
+	qsrand((uint)time.msec());
 }
 
 void Robot::step()
 {
-	int val1 = VISUALISER->ui->direction1->value();
-	int val2 = VISUALISER->ui->direction2->value();
+	int val1, val2;
 
-	int min = VISUALISER->ui->direction1->minimum();
-	int max = VISUALISER->ui->direction1->maximum();
+	if(VISUALISER->ui->controlDirections->isChecked()){
+		val1 = VISUALISER->ui->direction1->value();
+		val2 = VISUALISER->ui->direction2->value();
+	}else{
+		val1 = qrand()%360;
+		val2 = qrand()%360;
+	}
 
+		qreal pi = 3.1415;
 
-	qreal pi = 3.1415;
+		qreal alfa1 = 2.0*pi*(qreal(val1)/360.0);
+		qreal alfa2 = 2.0*pi*(qreal(val2)/360.0);
 
-	qreal alfa1 = pi*qreal(val1)/(max-min);
-	qreal alfa2 = pi*qreal(val2)/(max-min);
+		qreal dx1 = -sin(alfa1);
+		qreal dy1 = cos(alfa1);
 
-	qreal dx1 = -cos(alfa1);
-	qreal dy1 = -sin(alfa1);
-
-	qreal dx2 = -cos(alfa2);
-	qreal dy2 = -sin(alfa2);
+		qreal dx2 = -sin(alfa2);
+		qreal dy2 = cos(alfa2);
 
 	emit moves(QPointF(dx1, dy1), QPointF(dx2, dy2));
-
-	//emit moves(QPointF(0., 1.), QPointF(0., 1.));
 }
 
 void Robot::gaugeReaded(qreal gauge)
